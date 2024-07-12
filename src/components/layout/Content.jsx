853b51f2fc75033a635fbc983b5377e80ce63b45
maurilio.isolani <maurilio.isolani@partnerbank.com.br>
2024-07-12 07:41:39 -0300

    aplicação de estilos

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Content.css'
+
+const Content = props => (
+        <main className="Content">
+            <h1>Componente Content</h1>
+        </main>
+)
+
+export default Content
